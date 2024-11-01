d28fb2e2a5677458b22026287322037a1094e4cf
franCordobaDeveloper <franCordobaDeveloper>
2024-11-01 11:06:29 -0300

    Trabajando con las query

--- Repositorios/ProductosRepository.cs
+++ Repositorios/ProductosRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 using tl2_tp5_2024_franCordobaDeveloper.models;
 using tl2_tp5_2024_franCordobaDeveloper.Repositorios.interfaces;
 
@@ -10,15 +11,31 @@ namespace tl2_tp5_2024_franCordobaDeveloper.Repositorios
     public class ProductosRepository : IProductoRepository
     {
 
-                                                                                                         
+        private readonly string CadenaDeConexion = "Data Source=db/Tienda.db;Cache=Shared";                                                                                    
         public void CrearProducto(Productos producto)
         {
-            var queryString = ""; 
+            var queryString = @"
+                INSERT INTO Productos (Descripcion, Precio)
+                Values($descripcion, $precio) "; 
+            using (SqliteConnection connection = new SqliteConnection(CadenaDeConexion))
+            {
+                var command = new SqliteCommand(queryString, connection);
+                command.Parameters.AddWithValue("$descripcion", producto.Descripcion);
+                command.Parameters.AddWithValue("$precio", producto.Precio);
+        
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            
         }
 
         public void EliminarProductoPorId(int id)
         {
-            throw new NotImplementedException();
+            var queryString = @"
+            DELETE FROM Productos
+            WHERE idProducto = $id;
+            ";
+            
         }
 
         public List<Productos> ListarProductos()
